95e19f28a863058625e44b350438b833f4d71751
Josh Anderson <anderj93@ferris.edu>
2022-01-13 21:01:34 -0500

    Lab Week 1 - Unity Demo
    Updated now that I watched the video.

--- UnityProject/Assets/Scripts/LerpDemo.cs
+++ UnityProject/Assets/Scripts/LerpDemo.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class LerpDemo : MonoBehaviour
 {
+
+    public Transform pointA;
+    public Transform pointB;
+
+    [Range(0,1)]
+    public float percent = 0;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //void Start()
+    //{
+
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
+        DoInterpolation();
+    }
+    void DoInterpolation() {
+
+        if (pointA == null) return;
+        if (pointB == null) return;
+
+        Vector3 pos = Vector3.Lerp(pointA.position, pointB.position, percent);
+
+        Quaternion rot = Quaternion.Lerp(pointA.rotation, pointB.rotation, percent);
+
+        transform.position = pos;
+        transform.rotation = rot;
+            }
+        private void OnValidate()
+    {
+        DoInterpolation();
     }
 }
